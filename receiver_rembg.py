aebb4f6dc6bde470d5c535a14b541f12f52c1f9b
Thanakorn  Sappakit <ice.tennison@gmail.com>
2023-10-29 05:22:31 +0700

    update receiver rembg

--- receiver_rembg.py
+++ receiver_rembg.py
@@ -8,6 +8,8 @@ import traceback
 import numpy as np
 import datetime
 import os
+from rembg import remove
+import cv2
 
 spawning_folder = "data/ghost/spawning"
 
@@ -28,7 +30,7 @@ while True:
     # server.sendMsg(conn, response.encode())
 
     data = json.loads(server.recvMsg(conn))
-    if data:
+    if data.get("task") == "spawn":
         current_time = datetime.datetime.now()
 
         # Format the current time as a string
@@ -43,6 +45,34 @@ while True:
 
         print(f"File '{file_name}' created.")
         json_file.close()
+        
+    elif data.get("task") == "rembg":
+        frame = np.array(data.get("img"), dtype="uint8")
+        output = remove(frame)
+        non_empty_coordinates = np.argwhere(output[:, :, 3] > 0)
+        x, y, w, h = cv2.boundingRect(non_empty_coordinates)
+
+        cropped = output[x:x+w, y:y+h]
+        side_length = max(cropped.shape[0], cropped.shape[1])
+
+        square_image = np.zeros((side_length, side_length, 4), dtype=np.uint8)
+
+        x_offset = (side_length - cropped.shape[1]) // 2
+        y_offset = (side_length - cropped.shape[0]) // 2
+
+        square_image[y_offset:y_offset+cropped.shape[0], x_offset:x_offset+cropped.shape[1]] = cropped
+
+        b, g, r, a = cv2.split(square_image)
+
+        # Merge the channels into RGBA format
+        rgba_image = cv2.merge((r, g, b, a))
+        rgba_image_rotated = np.transpose(rgba_image, (1, 0, 2))
+
+        response = dict()
+        response['img'] = rgba_image_rotated.tolist()
+
+        server.sendMsg(conn, json.dumps(response))
+
 
     # Receive data from the client
     # data = server.recvMsg(conn)
